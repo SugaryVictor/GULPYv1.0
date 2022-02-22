40dc18684768c5b8683ece79c4b8c959a5cbad22
Victor <victorsugary@gmail.com>
2022-02-22 14:57:33 +0500

    Correct script.js

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -5,7 +5,7 @@ import concat from 'gulp-concat';
 export const scripts = () => {
     return gulp
         .src(['./app/js/script.js'])
-        .pipe(concat('scripe.min.js'))
+        .pipe(concat('script.min.js'))
         .pipe(gulp.dest('./app/js'))
         .pipe(browserSync.stream());
 };
