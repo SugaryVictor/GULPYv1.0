cce55056df24153fbfec379fbc34cca250d9b88f
Victor <victorsugary@gmail.com>
2022-02-27 11:40:11 +0500

    Edit task 'script'

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -4,8 +4,8 @@ import concat from 'gulp-concat';
 
 export const scripts = () => {
     return gulp
-        .src(['./app/js/script.js'])
-        .pipe(concat('script.min.js'))
+        .src(['./app/js/bundle.js'])
+        .pipe(concat('bundle.js'))
         .pipe(gulp.dest('./app/js'))
         .pipe(browserSync.stream());
 };
